af81f6edd04335032a73b218d4e4b185d0cfe0ba
César Higashi <76760288+higashi04@users.noreply.github.com>
2022-11-09 22:39:35 -0600

    finished sign up proccess

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,21 +1,48 @@
 import React from "react";
 import { useState } from "react";
 import "./SignUp.css";
-
+const lowerCaseLetterRegex = /[a-z]/g;
+const upperCaseLetterRegex = /[A-Z]/g;
 const SignUp = () => {
   const [username, setUsername] = useState(null);
   const [password, setPassword] = useState(null);
   const [email, setEmail] = useState(null);
   const [firstName, setFirstName] = useState(null);
   const [lastName, setLastName] = useState(null);
-  const [dateOfBirth, setDateOfBirth] = useState(null);
-  const [profileType, setProfileType] = useState("shit");
+  const [profileType, setProfileType] = useState("Student");
 
   const handleRadio = (event) => {
     
     setProfileType(event)
-    console.log(profileType)
   };
+  const handleSignUp = async() =>{
+    try {
+      console.log("btn clicked")
+      const data = {
+          username: username,
+          password: password,
+          email: email,
+          firstName: firstName,
+          lastName: lastName,
+          profileType: profileType,
+      }
+      if(username === null) {
+        throw new Error("Agregar nombre usuario");
+      }
+      if(password.match(lowerCaseLetterRegex) && !password.match(upperCaseLetterRegex)) {
+        alert("Contraseña no valida, agregar al menos una mayuscula");
+        throw new Error("password invalid");
+      }
+      await fetch("http://localhost:8080/usuario/registrarse", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'
+      },
+        body: JSON.stringify(data)
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <>
       <div className="container my-5 signUp-bg">
@@ -114,25 +141,6 @@ const SignUp = () => {
           </div>
         </div>
         <div className="row mb-3">
-          <div className="col">
-            <div className="form-floating">
-              <input
-                className="form-control"
-                placeholder="Fecha de Nacimiento"
-                name="dateOfBirth"
-                id="dateOfBirth"
-                type="date"
-                onInput={(event) => {
-                  setDateOfBirth(event.target.value);
-                }}
-              />
-              <label htmlFor="dateOfBirth" className="form-label">
-                Fecha de Nacimiento
-              </label>
-            </div>
-          </div>
-        </div>
-        <div className="row mb-3">
           <h4>Seleccione el tipo de perfil</h4>
           <div className="signUp-radios">
             <div className="form-check mx-5">
@@ -141,6 +149,7 @@ const SignUp = () => {
                 type="radio"
                 name="profile"
                 id="Maestro"
+                onClick={()=> handleRadio("Teacher")}
               />
               <label className="form-check-label" htmlFor="Maestro" value="Teacher" onClick={()=> handleRadio("Teacher")}>
                 Maestro
@@ -152,12 +161,17 @@ const SignUp = () => {
                 type="radio"
                 name="profile"
                 id="Alumno"
+                checked
+                onClick={()=> handleRadio("Student")}
               />
               <label className="form-check-label" htmlFor="Alumno" value="Student" onClick={()=> handleRadio("Student")}>
                 Alumno
               </label>
             </div>
           </div>
+        </div>
+        <div className="row mb-3">
+          <button className="btn btn-primary" onClick={() => handleSignUp()}>Submit</button>
         </div>
       </div>
       
